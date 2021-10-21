0bb5e577df9bfb2f3f6160f6e58944cf4d318370
Anastasiya Ponkrashenkova <ponkrashenkova@gmail.com>
2021-10-21 23:04:02 +0300

    Add attack variables

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const $chat = document.querySelector('.arenas');
 const $randomButton = document.querySelector('.button');
 const $reloadBtn = createElement('div', 'reloadWrap');
+const $formFight = document.querySelector('.control');
+
+const HIT = {
+    head: 30,
+    body: 25,
+    foot: 20,
+}
+const ATTACK = ['head', 'body', 'foot'];
 
 const player1 = {
     player: 1,
@@ -11,9 +19,9 @@ const player1 = {
     attack: function () {
         console.log(player1.name + ' Fight...')
     },
-    changeHp: changeHp,
-    renderHp: renderHp,
-    elHp: elHp
+    changeHp,
+    renderHp,
+    elHp
 }
 
 const player2 = {
@@ -25,9 +33,9 @@ const player2 = {
     attack: function () {
         console.log(player2.name + ' Fight...')
     },
-    changeHp: changeHp,
-    renderHp: renderHp,
-    elHp: elHp
+    changeHp,
+    renderHp,
+    elHp
 }
 
 function createElement(tag, className) {
@@ -106,13 +114,28 @@ $reloadBtn.addEventListener('click', function () {
     window.location.reload();
 })
 
-$randomButton.addEventListener('click', function () {
-    player1.changeHp(getRandom(20));
-    player1.renderHp();
-    player2.changeHp(getRandom(20));
-    player2.renderHp()
+// $randomButton.addEventListener('click', function () {
+//     player1.changeHp(getRandom(20));
+//     player1.renderHp();
+//     player2.changeHp(getRandom(20));
+//     player2.renderHp()
+//
+//
+//     if(player1.hp == 0 || player2.hp === 0) {
+//         $randomButton.disabled = true;
+//         createReloadButton();
+//     }
+//
+//     if(player1.hp === 0 && player1.hp < player2.hp) {
+//         $chat.appendChild(playerWins(player2.name));
+//     } else if(player2.hp === 0 && player2.hp < player1.hp) {
+//         $chat.appendChild(playerWins(player1.name));
+//     } else if(player1.hp === 0 && player2.hp === 0) {
+//         $chat.appendChild(playerWins());
+//     }
+// })
 
-
+function checkWin() {
     if(player1.hp == 0 || player2.hp === 0) {
         $randomButton.disabled = true;
         createReloadButton();
@@ -125,7 +148,52 @@ $randomButton.addEventListener('click', function () {
     } else if(player1.hp === 0 && player2.hp === 0) {
         $chat.appendChild(playerWins());
     }
-})
+}
+
+function enemyAttack() {
+    const hit = ATTACK[getRandom(3) - 1];
+    const defence = ATTACK[getRandom(3) - 1];
+
+    return {
+        value: getRandom(HIT(hit)),
+        hit,
+        defence,
+    }
+}
+
+$formFight.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const enemy = enemyAttack();
+    const attack = {};
+
+    for (const item of $formFight) {
+        if (item.checked && item.name === 'hit') {
+            attack.value = getRandom(HIT[item.value]);
+            attack.hit = item.value;
+        }
+        if (item.checked && item.name === 'defence') {
+            attack.defence = item.value;
+        }
+        item.checked = false;
+    }
+    getDamage(player1, checkAttack(enemy, attack));
+    getDamage(player2, checkAttack(attack, enemy));
+    checkWin();
+});
+
+function checkAttack(enemy, attack) {
+    let hit = 0;
+    if (attack.hit !== enemy.defence) {
+        hit = attack.value;
+    }
+    return hit;
+}
+
+function getDamage(player, damage) {
+    player.changeHp(damage);
+    player.renderHp();
+}
 
 $chat.appendChild(createPlayer(player1));
 $chat.appendChild(createPlayer(player2));
+
