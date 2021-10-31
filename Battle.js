21a604fad380f14ee6e49e38ddb9d11df090b21f
Anastasiya Ponkrashenkova <ponkrashenkova@gmail.com>
2021-10-31 22:22:02 +0300

    Add player selection

--- Battle.js
+++ Battle.js
@@ -1,6 +1,9 @@
-import { getRandom } from './Random.js';
-
-const $chat = document.querySelector('.chat');
+const ATTACK = ['head', 'body', 'foot'],
+    HIT = {
+        head: 30,
+        body: 25,
+        foot: 20,
+    };
 const logs = {
     start: 'Часы показывали [time], когда [player1] и [player2] бросили вызов друг другу.',
     end: [
@@ -40,51 +43,4 @@ const logs = {
     ],
     draw: ['Ничья - это тоже победа! ', 'В этом поединке нет победителя! '],
 };
-export function battleLogs(type, player1, player2, damage) {
-    const today = new Date(),
-        time = today.toLocaleTimeString(),
-        startTime = today.toLocaleTimeString().slice(0, -3),
-        { name: name1, hp: hp1 } = player1,
-        { name: name2, hp: hp2 } = player2,
-        random = getRandom(logs[type].length) - 1;
-    switch (type) {
-        case 'start':
-            const textStart = logs[type]
-                .replace('[time]', startTime)
-                .replace('[player1]', name1)
-                .replace('[player2]', name2);
-            const startEl = `<p>${textStart}</p>`;
-            $chat.insertAdjacentHTML('afterbegin', startEl);
-            break;
-        case 'hit':
-            const textHit = logs[type][random]
-                .replace('[playerKick]', name1)
-                .replace('[playerDefence]', name2);
-            const hit = `<span class='red'>-${damage}</span>`;
-            const elHit = `<p>${time} ${textHit} ${hit}  ${hp2}/100</p>`;
-            $chat.insertAdjacentHTML('afterbegin', elHit);
-            const $spanHit = document.querySelector('.red');
-            $spanHit.style.color = '#cd0e03';
-            break;
-        case 'defence':
-            const textDef = logs[type][random]
-                .replace('[playerKick]', name1)
-                .replace('[playerDefence]', name2);
-            const elDef = `<p>${time} ${textDef} <span class='blue'>-${damage}</span>  ${hp2}/100</p>`;
-            $chat.insertAdjacentHTML('afterbegin', elDef);
-            const $pDef = document.querySelector('.blue');
-            $pDef.style.color = '#0431f9';
-            break;
-        case 'end':
-            const textEnd = logs[type][random]
-                .replace('[playerWins]', name1)
-                .replace('[playerLose]', name2);
-            const endEl = `<p>${textEnd}</p>`;
-            $chat.insertAdjacentHTML('afterbegin', endEl);
-            break;
-        case 'draw':
-            const drawEl = logs[type][random];
-            $chat.insertAdjacentHTML('afterbegin', drawEl);
-            break;
-    }
-}
+export { ATTACK, HIT, logs };
